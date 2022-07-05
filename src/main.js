99befcd50fba6bfb2f98cf7ce37ea487f656daeb
lurx98 <lurx98@gmail.com>
2022-07-05 16:21:58 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 加载全局样式  【增加这一句】
+import './styles/index.less'
+import 'amfe-flexible'
+import '@/vant'
 Vue.config.productionTip = false
+
+
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
-
+// 路由映射数组
+const routes = []
+// 实例化路由对象
 const router = new VueRouter({
   routes
 })
